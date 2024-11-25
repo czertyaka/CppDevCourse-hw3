8401141ff27131709270b32239b5dca0398e1f05
vika00224 <i-vika00@mail.ru>
2024-11-26 01:22:04 +0500

    Changed two-sum.cpp

--- src/two-sum.cpp
+++ src/two-sum.cpp
@@ -15,17 +15,3 @@ bool two_sum(const int nums[ARRAY_SIZE], const int target, std::size_t& index0, 
     }
     return false; // Пара не найдена
 }
-
-int main() {
-    int nums[ARRAY_SIZE] = {2, 7, 11, 15, 3}; // Пример массива
-    int target = 10; // Целевое значение
-    std::size_t index0, index1;
-
-    if (two_sum(nums, target, index0, index1)) {
-        std::cout << "Indices found: " << index0 << " and " << index1 << std::endl;
-    } else {
-        std::cout << "No valid indices found." << std::endl;
-    }
-
-    return 0;
-}
