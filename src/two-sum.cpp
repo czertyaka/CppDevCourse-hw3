9bcdcc05f842232f0d83265a870e2e70b1b40832
inishevv <einishev@bk.ru>
2024-11-27 23:23:39 +0500

    added two-sum.cpp

---
+++ src/two-sum.cpp
@@ -0,0 +1,21 @@
+#include "two-sum.hpp"
+
+#define APPAY_SIZE 5
+
+bool two_sum(
+    const int nums[ARRAY_SIZE],
+    const int target,
+    std::size_t& index0,
+    std::size_t& index1
+) {
+    for (std::size_t i = 0; i < ARRAY_SIZE; ++i) {
+        for (std::size_t j = i + 1; j < ARRAY_SIZE; ++j) {
+            if (nums[i] + nums[j] == target) {
+                index0 = i;
+                index1 = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
