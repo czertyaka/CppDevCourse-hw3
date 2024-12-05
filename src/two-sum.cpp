3dd971d25efa6323a236889e6a7daa05d39886d0
Shveykina <catonmorgan@gmail.com>
2024-12-05 17:40:48 +0500

    Update two-sum.cpp

--- src/two-sum.cpp
+++ src/two-sum.cpp
@@ -1,20 +1,29 @@
+#include <iostream>
 #include <cstddef>
-#include "two-sum.hpp"
-#define ARRAY_SIZE 5
+#include <charconv>
+#include <string>
+#include <system_error>
+#include <stdexcept>
 
+#include "two-sum.hpp"
+
+#define ARRAY_SIZE 5
 bool two_sum(
-  const int nums[ARRAY_SIZE],
-  const int target,
-  std::size_t& index0,
-  std::size_t& index1
-){
-  for(int i = 0; i < ARRAY_SIZE - 1; i++){
-    for(int j = i + 1; j < ARRAY_SIZE; j++){
-      if(nums[i] + nums[j] == target){
+    const int nums[ARRAY_SIZE],
+    const int target,
+    std::size_t &index0,
+    std::size_t &index1)
+{
+  for (std::size_t i = 0; i < ARRAY_SIZE; ++i)
+  {
+    for (std::size_t j = i + 1; j < ARRAY_SIZE; ++j)
+    {
+      if (nums[i] + nums[j] == target)
+      {
         index0 = i;
         index1 = j;
-        return 1;
-              }
+        return true;
+      }
     }
   }
   return false;
