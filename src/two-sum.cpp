1d7de7175912cb2884c06c8db63d9d4eaa057940
ciigann <maxvetrov2004@ya.ru>
2024-12-05 18:29:31 +0500

    добавлена функция two_sum.

--- src/two-sum.cpp
+++ src/two-sum.cpp
@@ -1,20 +1,15 @@
-#include <unordered_map>
-
-#define ARRAY_SIZE 5
+#include "two-sum.hpp"
+#include <cstddef>
 
 bool two_sum(const int nums[ARRAY_SIZE], const int target, std::size_t& index0, std::size_t& index1) {
-    std::unordered_map<int, std::size_t> num_indices;
     for (std::size_t i = 0; i < ARRAY_SIZE; ++i) {
-        int complement = target - nums[i];
-        if (num_indices.find(complement) != num_indices.end()) {
-            index0 = num_indices[complement];
-            index1 = i;
-            if (index0 > index1) {
-                std::swap(index0, index1);
+        for (std::size_t j = i + 1; j < ARRAY_SIZE; ++j) {
+            if (nums[i] + nums[j] == target) {
+                index0 = i;
+                index1 = j;
+                return true;
             }
-            return true;
         }
-        num_indices[nums[i]] = i;
     }
     return false;
 }
