90eaa7276973cccfb031fbbe589a6ae9aaa0c2aa
A1r3t0 <113704274+A1r3t0@users.noreply.github.com>
2024-12-06 18:23:39 +0500

    Create two-sum.cpp

---
+++ src/two-sum.cpp
@@ -0,0 +1,20 @@
+#define ARRAY_SIZE 5
+
+bool two_sum(
+    const int nums[ARRAY_SIZE],
+    const int target,
+    std::size_t& index0,
+    std::size_t& index1
+) {
+    std::unordered_map<int, std::size_t> map;
+    for (int i = 0; i < ARRAY_SIZE; ++i) {
+        int complement = target - nums[i];
+        if (map.find(complement) != map.end()) {
+            index0 = std::min(map[complement], i);
+            index1 = std::max(map[complement], i);
+            return true;
+        }
+        map[nums[i]] = i;
+    }
+    return false;
+}
