c0530666d387400d234e133bcc96b8a8757c5d13
jonyks <146552678+jonyks@users.noreply.github.com>
2024-12-08 18:40:27 +0500

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/CppDevCourse-hw3
-# Build directory: /home/user/CppDevCourse-hw3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/user/CppDevCourse-hw3/build/two-sum-test[1]_include.cmake")
-add_test(two-sum-test "/home/user/CppDevCourse-hw3/build/two-sum-test")
-set_tests_properties(two-sum-test PROPERTIES  _BACKTRACE_TRIPLES "/home/user/CppDevCourse-hw3/CMakeLists.txt;20;add_test;/home/user/CppDevCourse-hw3/CMakeLists.txt;0;")
