cc8e7bccf1a5fba0f77cd57eb913a3dcd8af0804
alkaphran <alkaphran@gmail.com>
2024-12-08 20:35:01 +0500

    added two-sum.cpp

--- src/two-sum.cpp
+++ src/two-sum.cpp
@@ -4,7 +4,7 @@
 
 bool two_sum( const int nums[ARRAY_SIZE], const int target, std::size_t& index0, std::size_t& index1)
 {
-    for(int i = 0; i < ARRAY_SIZE - 1; i++)
+    for(int i = 0; i <= ARRAY_SIZE - 1; i++)
     {
         for(int j = i + 1; j < ARRAY_SIZE - 1; j++)
         {
