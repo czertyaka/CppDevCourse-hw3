6536afd2884527e2da7d59c2c89e8e7a96785034
Панин Максим <geidemaxim@gmail.com>
2024-12-10 18:04:58 +0500

    Update two-sum.cpp

--- src/two-sum.cpp
+++ src/two-sum.cpp
@@ -1,39 +1,22 @@
-#include <iostream>
-#define ARRAY_SIZE 5
+#include "two-sum.hpp"
 
-bool two_sum(
-	const int nums[ARRAY_SIZE],
+bool two_sum(const int nums[ARRAY_SIZE],
 	const int target,
 	std::size_t& index0,
 	std::size_t& index1
 )
 {
-	unsigned int kolvo_par = 0;
-	bool proverka_par = false;
-	for (unsigned int i = 0; i <= ARRAY_SIZE - 1; i++)
+	for (unsigned int i = 0; i < ARRAY_SIZE; i++)
 	{
 		for (unsigned int j = i + 1; j <= ARRAY_SIZE; j++)
 		{
 			if (nums[i] + nums[j] == target)
 			{
-				kolvo_par++;
-				if (kolvo_par == 1)
-				{
 					index0 = i;
 					index1 = j;
-					//return true;
-				}
-				else index1 = j;
-			 proverka_par = true;
+					return true;
 			}
 		}
 	}
-	if (proverka_par) 
-	{
-		return true;
-	}
-	else 
-	{	
-		return false;
-	}
+	return false;
 }
