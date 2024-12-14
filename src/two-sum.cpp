7a6dcd90dfc366989cfec82f0c9b8acb50daa868
annas-creator <costrikina@gmail.com>
2024-12-14 13:41:44 +0500

    added main.cpp

--- src/two-sum.cpp
+++ src/two-sum.cpp
@@ -4,7 +4,7 @@
 bool two_sum(const int nums[ARRAY_SIZE],const  int target, std::size_t& index0, std::size_t& index1) {
   for (int i = 0; i < ARRAY_SIZE; i++) 
   {
- for (int j = 1; j < ARRAY_SIZE; j++)
+ for (int j = (i+1); j < ARRAY_SIZE; j++)
    {
       if (nums[i] + nums[j] == target) {
          index0 = i; index1 = j;
